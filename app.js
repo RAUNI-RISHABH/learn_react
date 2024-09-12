d3d82757289e023964978a2fe11103e6188d0e3b
rishabh174 <rishabh.prakash42@gmail.com>
2024-09-12 22:36:28 +0530

    ep-2 igniting our app

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!");
+
+
+
+/** 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>
+ *              some text
+ *          </h1>
+ *      </div>
+ * </div>
+ * reactElement(object) => HTML(Browser understand)
+ * 
+ * if siblings add them in array so that both elements behaves like sibling
+ */
+
+// const parent = react.createElement("div", {id: "parent"},  react.createElement("div", {id: "child"},  react.createElement("h1", {id: "innerheaderofchild"}, "some inner text of h1" )))
+
+// will log react element which is a h1 type element which eventually is a javascript object with many properties
+//   2nd parameter is attribute, third is children
 console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
